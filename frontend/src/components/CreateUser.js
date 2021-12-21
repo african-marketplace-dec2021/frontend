24d9824c2897010e23ed7521c64e808e311af516
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-12-21 15:27:11 -0600

    user registration form bare bones

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,8 +1,67 @@
 import React from "react";
+import FormSchema from './FormSchema';
 
 function CreateUser () {
     return (
-        <p>We'll create a form for new users</p>
+        <form className='form container'>
+            {/* dont forget submit button */}
+            <h2> Register a new user!</h2>
+
+            <label>Full Name
+                <input
+                //   value={values.fullName}
+                //   onChange={onChange}
+                  name='fullname'
+                  type='text'
+                />
+            </label>
+
+            <label>Email
+                <input
+                    // value={values.email}
+                    // onChange={onChange}
+                    name='email'
+                    type='email'
+                />
+            </label>
+
+            <label>Username
+                <input
+                    // value={values.email}
+                    // onChange={onChange}
+                    name='username'
+                    type='text'
+                />
+            </label>
+
+            <label>Password
+                <input
+                    // value={values.email}
+                    // onChange={onChange}
+                    name='password'
+                    type='password'
+                />
+            </label>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <button id='registerButton'>Register</button>
+        </form>
     )
 }
 
