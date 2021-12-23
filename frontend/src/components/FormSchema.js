de94d622b97a356e495bd385064e6a381c8ce2d8
Allison Stewart <91036593+am-stewart@users.noreply.github.com>
2021-12-22 23:58:44 -0500

    new listing form validation mostly functional (#26)

--- frontend/src/components/FormSchema.js
+++ frontend/src/components/FormSchema.js
@@ -23,7 +23,23 @@ const formSchema = yup.object().shape({
     .min(5, 'Password must be at least 5 characters!'),
   tos: yup
     .boolean()
-    .oneOf([true], 'You must accept Terms of Service!')
+    .oneOf([true], 'You must accept Terms of Service!'),
+    name: yup
+    .string()
+    .required('You must enter a name for your item')
+    .min(2, 'Item name must be a minimum of 2 characters'),
+  description: yup
+    .string()
+    .required('You must enter a description for your item'),
+  price: yup
+    .number()
+    .required('You must set a price for your item'),
+  location: yup
+    .string()
+    .oneOf(['Location 1', 'Location 2', 'Location 3', 'Location 4', 'Location 5'], 'You must choose a location'),
+  category: yup
+    .string()
+    .oneOf(['1', '2', '3', '4', '5'], 'You must choose a category')
 });
 
 
